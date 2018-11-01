03f728d1da04e4782746ff23ea38b7a9b7cb163d
eggtargaryen <793330670@qq.com>
2018-11-01 23:06:00 +0800

    Site updated: 2018-11-01 23:06:00

--- js/diaspora.js
+++ js/diaspora.js
@@ -490,6 +490,6 @@ $(function() {
     if (comment.data('ae') == true){
         comment.click();
     }
-    console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/Fechin/hexo-theme-diaspora")
+    console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/eggtargaryen")
 })
 
